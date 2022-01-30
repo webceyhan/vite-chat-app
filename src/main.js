8bb123b32cecdc1cfa006e025cfada8a1a787881
Evren Ceyhan <evren@ceyhan.io>
2022-01-30 14:02:58 +0300

    bootstrap added

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+// load bootstrap
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-createApp(App).mount('#app')
+import { createApp } from 'vue';
+import App from './App.vue';
+
+createApp(App).mount('#app');
